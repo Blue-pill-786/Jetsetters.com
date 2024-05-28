1fb2a8dfdbd17ecbf413041636ecc3d68e02606d
Akash kumar <94979388+iakash22@users.noreply.github.com>
2024-05-28 23:58:08 +0530

    scroll top fix

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
     if (!sessionStorage.getItem('token')) {
       setTokenToSession();
     }
-    // window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
   }, [pathname]);
   return (
     <div className='App overflow-x-hidden'>
